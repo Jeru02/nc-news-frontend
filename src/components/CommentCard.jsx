4d93c58ffea87f3c8701f224577352f6f6845e80
Jeru02 <boatengj006472@gmail.com>
2025-05-23 16:51:03 +0100

    create delete button

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,4 +1,9 @@
-const CommentCard = ({singleComment})=>{
+import DeleteCommentButton from "./DeleteCommentButton"
+
+const CommentCard = ({singleComment, setComments})=>{
+
+
+   
 
    return <li key={singleComment.comment_id} >
       
@@ -6,6 +11,7 @@ const CommentCard = ({singleComment})=>{
       <p>{singleComment.body}</p>
       <p>{singleComment.votes}</p>
       <p>{singleComment.created_at}</p>
+      <DeleteCommentButton comment_id={singleComment.comment_id} setComments={setComments}/>
     </li>
 
 
