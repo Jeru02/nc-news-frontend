7ff66308bd77f341618611d7ffd983ec4d8ba6f0
Jeru02 <boatengj006472@gmail.com>
2025-05-28 00:56:08 +0100

    adjust delete button for current users post only

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,17 +1,20 @@
 import DeleteCommentButton from "./DeleteCommentButton"
-
+import { useContext } from "react";
+import { AccountContext } from "../Account";
 const CommentCard = ({singleComment, setComments})=>{
+   console.log(singleComment, "<-------- JHEREEE")
 
 
-   
+   const { loggedInUser } = useContext(AccountContext);
 
+   //if single comment.author = current user add delete button
    return <li key={singleComment.comment_id} >
       
       <p>{singleComment.author}</p>
       <p>{singleComment.body}</p>
       <p>{singleComment.votes}</p>
       <p>{singleComment.created_at}</p>
-      <DeleteCommentButton comment_id={singleComment.comment_id} setComments={setComments}/>
+      {singleComment.author === loggedInUser ? <DeleteCommentButton comment_id={singleComment.comment_id} setComments={setComments}/> : null}
     </li>
 
 
--- src/components/VoteButtons.jsx
+++ src/components/VoteButtons.jsx
@@ -1,20 +1,23 @@
-const voteButtons = ({setVote, setHasVoted, hasVoted})=>{
-    
-function incVote (){
-    setVote(1)
+const voteButtons = ({ setVote, setHasVoted, hasVoted }) => {
+  function incVote() {
+    setVote(1);
+    setHasVoted(true);
+  }
+  function decVote() {
+    setVote(-1);
+    setHasVoted(true);
+  }
 
-    setHasVoted(true)
-}
-function decVote (){
+  return (
+    <>
+      <button onClick={incVote} disabled={hasVoted}>
+        Like
+      </button>
+      <button onClick={decVote} disabled={hasVoted}>
+        dislike
+      </button>
+    </>
+  );
+};
 
-     setVote(-1)
-    setHasVoted(true)
-}
-
-
-
-return <><button onClick={incVote} disabled={hasVoted}>Like</button>
-<button onClick={decVote} disabled={hasVoted}>dislike</button></>
-}
-
-export default voteButtons
+export default voteButtons;
